6c0699a9854d78c51d3ed2e782cc15983b4c7f0f
rsa153 <r-saber@nlaw.northwestern.edu>
2019-03-25 18:59:07 -0500

    customer db with email validation

--- Cupcakes/models/customer.js
+++ Cupcakes/models/customer.js
@@ -1,9 +1,37 @@
 module.exports = function(sequelize, DataTypes) {
   var Customer = sequelize.define("Customer", {
-    firstName: DataTypes.TEXT,
-    lastName: DataTypes.TEXT,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    firstName: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+    lastName: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      lowercase: true,
+      validate: {
+        len: [1, 140],
+        isEmail: true,
+        notEmpty: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      alidate: {
+        len: [1, 140]
+      }
+    }
   });
   return Customer;
 };
