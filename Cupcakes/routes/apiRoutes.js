4bbdd956571e67e2135e65e486cc970e11daa6cd
rsa153 <r-saber@nlaw.northwestern.edu>
2019-03-29 11:58:06 -0500

    updates to htmlroutes.js

--- Cupcakes/routes/apiRoutes.js
+++ Cupcakes/routes/apiRoutes.js
@@ -1,24 +1,34 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Find all Authors and return them to the user with res.json
+  app.get("/api/customers", function(req, res) {
+    db.Customers.findAll({}).then(function(dbCustomers) {
+      res.json(dbCustomers);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.get("/api/customers/:id", function(req, res) {
+    // Find one Author with the id in req.params.id and return them to the user with res.json
+    db.Customers.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbCustomers) {
+      res.json(dbCustomers);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/customers", function(req, res) {
+    // Create an Author with the data available to us in req.body
+    console.log(req.body);
+    db.Customers.create({
+      where: {
+        id: req.params.id,
+        firstName: req.params.firstName
+      }
+    }).then(function(dbCustomers) {
+      res.json(dbCustomers);
     });
   });
 };
