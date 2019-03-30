a1c113fbc6d51178a119f1db68e56ac24b67514e
T Saraya <saratgrffn@gmail.com>
2019-03-30 11:40:56 -0500

    added route for orders

--- Cupcakes/routes/apiRoutes.js
+++ Cupcakes/routes/apiRoutes.js
@@ -10,9 +10,7 @@ module.exports = function(app) {
   app.get("/api/customers/:id", function(req, res) {
     db.Customers.findOne({
       where: {
-        id: req.params.id,
-        email: req.params.email,
-        password: req.params.password
+        id: req.params.id
       }
     }).then(function(dbCustomers) {
       res.json(dbCustomers);
@@ -26,4 +24,31 @@ module.exports = function(app) {
       res.json(dbCustomers);
     });
   });
+
+  app.post("/api/orders", function(req,res) {
+    console.log(req.body);
+    
+    db.orders.create({
+      date: req.body.date,
+      billingAddress: req.body.billingAddress,
+      city: req.body.city,
+      state: req.body.state,
+      email: req.body.email,
+      cupcakeType: req.body.cupcakeType,
+      quantity: req.body.quantity,
+      specialInstructions: req.body.specialInstructions,
+      totalPrice: req.body.totalPrice,
+      card: req.body.card,
+      cardNumber: req.body.cardNumber,
+      cvc: req.body.cvc,
+      CustomerID: req.body.CustomerID
+    })
+      .then(function(dbOrders){
+      console.log(dbOrders);
+      res.json(dbOrders);
+    });
+  
+  });
+
+
 };
