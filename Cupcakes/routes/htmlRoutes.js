16a9dc38c291be62118d664f1b265a20be03a3c9
Alexis Reyes <Alex@alex-da-great.lan>
2019-03-31 14:51:07 -0500

    Name change

--- Cupcakes/routes/htmlRoutes.js
+++ Cupcakes/routes/htmlRoutes.js
@@ -1,29 +1,31 @@
 var path = require("path");
 
 module.exports = function(app) {
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/home.html"));
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/pages/home.html"));
   });
   //Carousel became about page
   // app.get("/about", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/views/about.html"));
+  //   res.sendFile(path.join(__dirname, "../public/pages/about.html"));
   // });
-  app.get("/inventory", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/inventory.html"));
-  });
+  // app.get("/inventory", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/pages/inventory.html"));
+  // });
 
   app.get("/order", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/order.html"));
-  });
-  app.get("/custom", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/customization.html"));
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/home.html"));
+    res.sendFile(path.join(__dirname, "../public/pages/order.html"));
   });
 };
+
+// Render 404 page for any unmatched routes
+//   app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/pages/home.html"));
+//   });
+// };
+
+// app.get("/custom", function(req, res) {
+//   res.sendFile(path.join(__dirname, "../public/pages/customization.html"));
+// });
 
 //OLD CODE
 // var db = require("../models");
@@ -31,24 +33,23 @@ module.exports = function(app) {
 // module.exports = function(app) {
 
 //   app.get("/", function(req, res) {
-//     res.sendFile(db.join(__dirname, "../views/home.html"));
+//     res.sendFile(db.join(__dirname, "../pages/home.html"));
 //   });
 //   app.get("/about", function(req, res) {
-//     res.sendFile(db.join(__dirname, "../views/about.html"));
+//     res.sendFile(db.join(__dirname, "../pages/about.html"));
 //   });
 //   app.get("/inventory", function(req, res) {
-//     res.sendFile(db.join(__dirname, "../views/inventory.html"));
+//     res.sendFile(db.join(__dirname, "../pages/inventory.html"));
 //   });
 
 //   app.get("/order", function(req, res) {
-//     res.sendFile(db.join(__dirname, "../views/order.html"));
+//     res.sendFile(db.join(__dirname, "../pages/order.html"));
 //   });
 //   app.get("/custom", function(req, res) {
-//     res.sendFile(db.join(__dirname, "../views/customization.html"));
+//     res.sendFile(db.join(__dirname, "../pages/customization.html"));
 //   });
 
 //   // Render 404 page for any unmatched routes
 //   app.get("*", function(req, res) {
 //     res.render("404");
 //   });
-// };
